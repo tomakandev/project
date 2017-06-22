84e1861c329fa33ff1eff1b937b30fa33935d8fb
tomakandev <tomakandev@gmail.com>
2017-06-22 21:03:19 +0200

    Create characterSpritesTable.lua

---
+++ characterSpritesTable.lua
@@ -0,0 +1,46 @@
+    characterSprite = {
+        male = {
+            dwarf = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            human = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            elf = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            undead = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            }
+        },
+        female = {
+            dwarf = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            human = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            elf = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            },
+            undead = {
+                warrior = "",
+                sorcerer = "",
+                rogue = ""
+            }
+        }
+    }
